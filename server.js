37f051f735ffc3dcd9a9a0dc73390e2668414d67
Kawashima <kazu12k@icloud.com>
2026-05-09 15:29:53 +0900

    first commit

--- server.js
+++ server.js
@@ -6,13 +6,21 @@ const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", ws => {
+  console.log("Client connected");
+
   ws.on("message", msg => {
     console.log("received:", msg);
     ws.send("echo: " + msg);
   });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
 });
 
+// ★ Render が割り当てるポートを必ず使う
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
